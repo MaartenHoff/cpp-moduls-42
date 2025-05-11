ba031ececd12f61e98b13ab656ac93f4871b6e75
MaartenHoff <maarten.berlin@gmail.com>
2025-05-11 11:28:42 +0200

    Module 02 ex02 complete

--- Module_02/ex02/Fixed.cpp
+++ Module_02/ex02/Fixed.cpp
@@ -73,82 +73,65 @@ bool Fixed::operator!=(const Fixed &other) const { return _value != other._value
 
 Fixed	Fixed::operator+(const Fixed &other) const
 {
-	Fixed	temp;
-
-	temp = this->_value + other._value;
-	return (temp);
+	int rawSum = _value + other._value;
+    return Fixed(rawSum);
 }
 
 Fixed	Fixed::operator-(const Fixed &other) const
 {
 	Fixed	temp;
 
-	temp = this->_value - other._value;
+	temp._value = this->_value - other._value;
 	return (temp);
 }
 
 Fixed	Fixed::operator*(const Fixed &other) const
 {
 	Fixed	temp;
+	long	rawProduct;
 
-	temp = this->_value * other._value;
+	rawProduct = (long)this->_value * other._value;
+	temp._value = rawProduct >> _fract_bits;
 	return (temp);
 }
 
 Fixed	Fixed::operator/(const Fixed &other) const
 {
 	Fixed	temp;
+	int		rawProduct;
 
-	temp = this->_value / other._value;
+	long numerator = (long)this->_value << _fract_bits;
+	rawProduct = numerator / other._value;
+	temp.setRawBits(rawProduct);
 	return (temp);
 }
 
-// *** EX02: Implementierung ***
+Fixed	&Fixed::operator++()
+{
+	this->_value++;
+	return (*this);
+}
 
-// Vergleichsoperatoren (vergleich Raw-Bits):
-// Schreibe jede Funktion als Einzeiler, z.B.:
-// bool Fixed::operator>(const Fixed &other) const {
-//     return this->_value > other._value;
-// }
+Fixed	Fixed::operator++(int)
+{
+	Fixed temp(*this);
+	this->_value++;
+	return(temp);
+}
 
-// Arithmetik: Beachte Skalierung mit _fract_bits:
-// - Option A: Konvertiere beide Operanden mit toFloat(), rechne, und baue Fixed zurück
-// - Option B: Rechne direkt mit _value (vorsichtig bei Überlauf)
-// Beispiel Gerüst:
-// Fixed Fixed::operator+(const Fixed &other) const {
-//     Fixed result;
-//     result._value = this->_value + other._value;
-//     return result;
-// }
+Fixed	&Fixed::operator--()
+{
+	this->_value--;
+	return (*this);
+}
+Fixed	Fixed::operator--(int)
+{
+	Fixed temp(*this);
+	this->_value--;
+	return(temp);
+}
 
-// Inkrement/Dekrement: Der kleinste Schritt ist 1 << 0 (also 1 im Raw-Format)
-// Pre-Inkrement:
-// Fixed &Fixed::operator++() {
-//     this->_value += 1;
-//     return *this;
-// }
-// Post-Inkrement:
-// Fixed Fixed::operator++(int) {
-//     Fixed temp(*this);
-//     ++(*this);
-//     return temp;
-// }
-
-// Ähnliche Umsetzung für --
-
-// min / max:
-// Wähle anhand von Vergleichsoperatoren:
-// Fixed &Fixed::min(Fixed &a, Fixed &b) {
-//     return (a < b) ? a : b;
-// }
-// const Fixed &Fixed::min(const Fixed &a, const Fixed &b) {
-//     return (a < b) ? a : b;
-// }
-// max analog.
-
-// -----
-// Deine Aufgabe:
-// 1. Übernimm die Deklarationen oben in Fixed.hpp.
-// 2. Schreibe die Methoden in Fixed.cpp anhand der Kommentare.
-// 3. Kompiliere und teste jeden Operator einzeln, um zu verstehen, wie er wirkt.
-// 4. Melde dich, wenn du bei einem spezifischen Operator unsicher bist!
+Fixed		&Fixed::min(Fixed &a, Fixed &b) { return (a < b ? a : b); }
+const Fixed	&Fixed::min(const Fixed &a, const Fixed &b) {return (a < b ? a : b); }
+Fixed		&Fixed::max(Fixed &a, Fixed &b) { return (a > b ? a : b); }
+const Fixed	&Fixed::max(const Fixed &a, const Fixed &b) {return (a > b ? a : b); }
