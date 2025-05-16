0594a13589b9e8398d86b9bae47f7290999fbd16
MaartenHoff <maarten.berlin@gmail.com>
2025-05-16 16:04:16 +0200

    03 ex01 done maybe more test need for 00 and 01

---
+++ Module_03/ex01/ClapTrap.cpp
@@ -0,0 +1,60 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name) 
+	: _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
+		std::cout << "ClapTrap " << _name << " constructed\n";
+	}
+ClapTrap::ClapTrap( const ClapTrap &other ) { 
+	*this = other; 
+	std::cout << "ClapTrap " << _name << " copy constructed\n";
+}
+ClapTrap	&ClapTrap::operator=( const ClapTrap &other )
+{
+	_name = other._name;
+	_hitPoints = other._hitPoints;
+	_energyPoints = other._energyPoints;
+	_attackDamage = other._attackDamage;
+	return (*this);
+}
+ClapTrap::~ClapTrap() {
+	std::cout << "ClapTrap " << _name << " destructed\n";
+}
+
+void	ClapTrap::attack(const std::string& target)
+{
+	if (!_energyPoints || !_hitPoints)
+	{
+		std::cout << "ClapTrap " << _name << " can't attack\n";
+		return ;
+	}
+	_energyPoints--;
+	std::cout << "ClapTrap " << _name << " attacks " << target
+              << ", causing " << _attackDamage << " points of damage!\n";
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	if (!_energyPoints || !_hitPoints)
+	{
+		std::cout << "No energy- or hit points left\n";
+		return ;
+	}
+	if (_hitPoints < amount)
+        _hitPoints = 0;
+	else
+		_hitPoints -= amount;
+	_energyPoints--;
+	std::cout << "ClapTrap " << _name << " takes " << amount << " damage!\n";
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (!_energyPoints || !_hitPoints)
+	{
+		std::cout << "ClapTrap " << _name << " can't repair\n";
+		return ;
+	}
+	_hitPoints += amount;
+	_energyPoints--;
+	std::cout << "ClapTrap " << _name << " repairs itself for " << amount << " HP\n";
+}
---
+++ Module_03/ex01/main.cpp
@@ -0,0 +1,12 @@
+#include "ScavTrap.hpp"
+
+int main() {
+	ScavTrap scav("Guardian");
+
+	scav.attack("Enemy");
+	scav.takeDamage(30);
+	scav.beRepaired(20);
+	scav.guardGate();
+
+	return 0;
+}
