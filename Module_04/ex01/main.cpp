00bf5b0c510f808c3a59ada0a8a12be68cf74fa1
Maarten Hoff <maahoff@c4a7c6.42berlin.de>
2025-06-25 16:46:22 +0200

    small fix on ex00 and idea for main in ex01

--- Module_04/ex01/main.cpp
+++ Module_04/ex01/main.cpp
@@ -1,7 +1,72 @@
 #include"Dog.hpp"
 #include"Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int main() {
-	
+	std::cout << "=== ex00 Tests ===" << std::endl;
+	const Animal* A = new Animal();
+	const Animal* D = new Dog();
+	const Animal* C = new Cat();
+	const WrongAnimal* WA = new WrongAnimal();
+	const WrongAnimal* WC = new WrongCat();
+
+	std::cout << "\n=== getType Tests ===" << std::endl;
+	std::cout << "D type: " << D->getType() << std::endl;
+	std::cout << "C type: " << C->getType() << std::endl;
+	std::cout << "A type: " << A->getType() << std::endl;
+	std::cout << "WA type: " << WA->getType() << std::endl;
+	std::cout << "WC type: " << WC->getType() << std::endl;
+
+	std::cout << "\n=== makeSound Tests ===" << std::endl;
+	C->makeSound();
+	D->makeSound();
+	A->makeSound();
+	WC->makeSound();
+	WA->makeSound();
+
+	std::cout << "\n=== Copy & Assignment Tests ===" << std::endl;
+	Cat originalCat;
+	Cat copiedCat(originalCat);
+	Cat assignedCat;
+	assignedCat = originalCat;
+
+	Dog originalDog;
+	Dog copiedDog(originalDog);
+	Dog assignedDog;
+	assignedDog = originalDog;
+
+	std::cout << "\n=== Destructors ex00 ===" << std::endl;
+	delete A;
+	delete D;
+	delete C;
+	delete WA;
+	delete WC;
+
+	std::cout << "\n=== ex01 Tests ===" << std::endl;
+	const int n = 4;
+	Animal* arr[n];
+	for (int i = 0; i < n; ++i) {
+		if (i < n/2)
+			arr[i] = new Dog();
+		else
+			arr[i] = new Cat();
+	}
+
+	std::cout << "\n=== Deleting array ===" << std::endl;
+	for (int i = 0; i < n; ++i)
+		delete arr[i];
+
+	std::cout << "\n=== Deep Copy Brain Test ===" << std::endl;
+	Dog dog1;
+	dog1.->ideas[0] = "Chase ball";
+	Dog dog2(dog1);
+	std::cout << "Dog1 idea[0]: " << dog1.getBrain()->ideas[0] << std::endl;
+	std::cout << "Dog2 idea[0]: " << dog2.getBrain()->ideas[0] << std::endl;
+	dog2.getBrain()->ideas[0] = "Eat";
+	std::cout << "After change:" << std::endl;
+	std::cout << "Dog1 idea[0]: " << dog1.getBrain()->ideas[0] << std::endl;
+	std::cout << "Dog2 idea[0]: " << dog2.getBrain()->ideas[0] << std::endl;
+
 	return 0;
 }
