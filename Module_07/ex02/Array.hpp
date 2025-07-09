c0866bf8372522837c314f4b8eb1db0cecc5b351
Maarten Hoff <maahoff@c4a7c6.42berlin.de>
2025-07-09 17:23:51 +0200

    cpp07 Makefile fix + ex02 structure/hpp

--- Module_07/ex02/Array.hpp
+++ Module_07/ex02/Array.hpp
@@ -1,1 +1,26 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <exception>
+
+template<typename T>
+class Array
+{
+	private:
+		T				*elements;
+		unsigned int	n;
+	
+	public:
+		Array();
+		Array( unsigned int n );
+		Array( Array const &other );
+		Array	&operator = ( Array const &other );
+		~Array();
+		
+		T		&operator [] ( int idx );
+		const T	&operator[](int idx) const;
+
+		unsigned int	size() const;
+};
+
+#include "Array.tpp"
--- Module_07/ex02/Array.tpp
+++ Module_07/ex02/Array.tpp
@@ -1,1 +1,3 @@
 #include "Array.hpp"
+
+Array<T>::Arr
