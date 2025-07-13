2cb0bb356a983028f26aa8672e0d25bc0fb2d298
Maarten Hoff <maahoff@c4a7c6.42berlin.de>
2025-07-13 16:55:35 +0200

    cpp08 ex00 complete but honestly a bit ugly

--- Module_08/ex00/easyfind.hpp
+++ Module_08/ex00/easyfind.hpp
@@ -1,4 +1,16 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <algorithm>
 
+template<typename T>
+typename T::iterator	easyfind( T &container, int value );
+
+class NotFoundException : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "No occurence was found!";
+	}
+};
+
+#include "easyfind.tpp"
--- Module_08/ex00/main.cpp
+++ Module_08/ex00/main.cpp
@@ -1,1 +1,29 @@
 #include "easyfind.hpp"
+#include <vector>
+
+int	main(void) {
+	std::vector<int> has_value;
+	has_value.push_back(1);
+	has_value.push_back(2);
+	has_value.push_back(3);
+	has_value.push_back(4);
+	has_value.push_back(5);
+
+	std::vector<int> no_value;
+	no_value.push_back(1);
+	no_value.push_back(2);
+	no_value.push_back(3);
+	no_value.push_back(5);
+	no_value.push_back(6);
+
+	try
+	{
+		std::cout << *easyfind(has_value, 4) << std::endl;
+		std::cout << *easyfind(no_value, 4) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return (0);
+}
